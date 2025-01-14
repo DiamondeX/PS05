3a6390bda62618a466e0c345c3f66f792a636373
DiamondeX <DiamondeX@users.noreply.github.com>
2025-01-14 21:33:56 +0100

    changed spider's crawled category

--- divanparse/divanparse/spiders/divannewparse.py
+++ divanparse/divanparse/spiders/divannewparse.py
@@ -4,7 +4,14 @@ import scrapy
 class DivannewparseSpider(scrapy.Spider):
     name = "divannewparse"
     allowed_domains = ["https://divan.ru"]
-    start_urls = ["https://www.divan.ru/category/divany-i-kresla"]
+    start_urls = ["https://www.divan.ru/category/svet"]
 
     def parse(self, response):
-        pass
+        divans = response.css('div._Ud0k')
+        for divan in divans:
+            yield {
+                'name': divan.css('div.lsooF span[itemprop=name]::text').get(),
+                'price': int(divan.css('div.lsooF meta[itemprop=price]').attrib['content']),
+                'url': divan.css('div.lsooF a.qUioe').attrib['href']
+            }
+
